30cd9f46b4144f59cad0a5175a3e858a217b8045
Brett Kuprel <brkuprel@gmail.com>
2024-04-04 13:28:52 -0700

    batch

--- depth_from_rgb.py
+++ depth_from_rgb.py
@@ -2,10 +2,8 @@ import argparse
 import cv2
 import time
 import math
-import numpy
 import torch
 import torchvision.transforms
-from torch import Tensor
 
 from depth_anything import DepthAnything
 
@@ -23,6 +21,7 @@ if __name__ == '__main__':
     depth_model = depth_model.to(device).eval()
 
     depth_height = 518
+    batch_size = 8
 
     path_rgb = args.video_path
     path_depth = path_rgb.replace('.mp4', '_depth.mp4')
@@ -31,10 +30,11 @@ if __name__ == '__main__':
     frame_width, frame_height = int(video_rgb.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video_rgb.get(cv2.CAP_PROP_FRAME_HEIGHT))
     depth_width = frame_width / frame_height * depth_height
     depth_width = math.ceil(depth_width / 14) * 14
-    print("Depth Width", depth_width)
+    print("Depth Shape", (depth_height, depth_width))
     # frame_rate = int(video_rgb.get(cv2.CAP_PROP_FPS))
     frame_rate = 30
     frame_count = int(video_rgb.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_count = frame_count // batch_size * batch_size
     video_depth = cv2.VideoWriter(path_depth, cv2.VideoWriter_fourcc(*"mp4v"), frame_rate, (depth_width, depth_height))
 
     msg = "Frame height: {}, Frame width: {}, Frame rate: {}, Frame count: {}"
@@ -44,33 +44,40 @@ if __name__ == '__main__':
     rgb_std = torch.tensor([0.229, 0.224, 0.225], device=device, dtype=torch.float32)
     rgb_resize = torchvision.transforms.Resize((depth_height, depth_width))
 
-    i, t0 = 0, time.time()
-    while video_rgb.isOpened():
-        i += 1
-        if i % 10 == 0:
+    t0 = time.time()
+    for i in range(frame_count):
+        if i % batch_size == 0:
             print(f'Frame {i} of {frame_count}, FPS: {i / (time.time() - t0):.2f}')
 
-        is_frame, frame_rgb = video_rgb.read()
-        if not is_frame: break
+        frames_rgb = []
+        for i in range(batch_size):
+            is_frame, frame_rgb = video_rgb.read()
+            if not is_frame: break
+            frames_rgb.append(frame_rgb)
 
-        # frame_rgb = cv2.cvtColor(frame_rgb, cv2.COLOR_BGR2RGB)
-        frame_rgb = torch.tensor(frame_rgb, device=device, dtype=torch.float32)
-        frame_rgb = frame_rgb.permute(2, 0, 1).flip(0)
-        frame_rgb = rgb_resize.forward(frame_rgb)
-        frame_rgb /= 255
-        frame_rgb -= rgb_mean[:, None, None]
-        frame_rgb /= rgb_std[:, None, None]
-        frame_rgb = frame_rgb.unsqueeze(0)
+        frames_rgb = torch.tensor(frames_rgb, device=device, dtype=torch.float32)
+        frames_rgb = frames_rgb.permute(0, 3, 1, 2).flip(1)
+
+        # frame_rgb = frame_rgb.permute(2, 0, 1).flip(0)
+        print(frames_rgb.shape)
+        frames_rgb = rgb_resize.forward(frames_rgb)
+        print(frames_rgb.shape)
+        frames_rgb /= 255
+        frames_rgb -= rgb_mean[None, :, None, None]
+        frames_rgb /= rgb_std[None, :, None, None]
 
         with torch.no_grad():
-            frame_depth = depth_model.forward(frame_rgb)
+            frames_depth = depth_model.forward(frames_rgb)
 
-        frame_depth -= frame_depth.min()
-        frame_depth /= frame_depth.max()
-        frame_depth = (frame_depth[0] * 255).to(torch.uint8).cpu().numpy()
-        frame_depth = cv2.cvtColor(frame_depth, cv2.COLOR_GRAY2BGR)
+        frames_depth -= frames_depth.min()
+        frames_depth /= frames_depth.max()
+        frames_depth *= 255
+        frames_depth = frames_depth.to(torch.uint8)
 
-        video_depth.write(frame_depth)
+        frames_depth = frames_depth.cpu().numpy()
+        for frame_depth in frames_depth:
+            frame_depth = cv2.cvtColor(frame_depth, cv2.COLOR_GRAY2BGR)
+            video_depth.write(frame_depth)
 
     video_rgb.release()
     video_depth.release()
