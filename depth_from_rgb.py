9daf9c0abbc688cb37a2adfe4bdad9f6753b4a34
Brett Kuprel <brkuprel@gmail.com>
2024-04-04 13:16:41 -0700

    revert

--- depth_from_rgb.py
+++ depth_from_rgb.py
@@ -53,8 +53,9 @@ if __name__ == '__main__':
         is_frame, frame_rgb = video_rgb.read()
         if not is_frame: break
 
+        frame_rgb = cv2.cvtColor(frame_rgb, cv2.COLOR_BGR2RGB)
         frame_rgb = torch.tensor(frame_rgb, device=device, dtype=torch.float32)
-        frame_rgb = frame_rgb.permute(2, 0, 1).flip(0)
+        frame_rgb = frame_rgb.permute(2, 0, 1)
         frame_rgb = rgb_resize.forward(frame_rgb)
         frame_rgb /= 255
         frame_rgb -= rgb_mean[:, None, None]
@@ -63,10 +64,9 @@ if __name__ == '__main__':
         frame_depth = depth_model.forward(frame_rgb)
         frame_depth -= frame_depth.min()
         frame_depth /= frame_depth.max()
-        frame_depth = (frame_depth[0] * 255).to(torch.uint8)
-
-        frame_depth = frame_depth.cpu().numpy()
+        frame_depth = (frame_depth[0] * 255).to(torch.uint8).cpu().numpy()
         frame_depth = cv2.cvtColor(frame_depth, cv2.COLOR_GRAY2BGR)
+
         video_depth.write(frame_depth)
 
     video_rgb.release()
